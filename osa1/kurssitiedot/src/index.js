7241be7d3d93df81f69902cf7d7808af1b2a0a92
Sami Åhl <sami.ahl@relex.fi>
2019-10-15 12:34:44 +0300

    1.4

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -3,32 +3,31 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     const course = 'Half Stack application development'
-    const partList = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
-    const exerciseList =[10, 7, 14]
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
 
     return (
         <div>
             <Header name={course}/>
-            <Content Content1={part1.name + " " + part1.exercises} Content2={part2.name + " "+ part2.exercises} Content3={part3.name + " "+ part3.exercises}/>
-            <Total exerciseSum={part1.exercises + part2.exercises + part3.exercises} />
+            <Content partList={parts} />
+            <Total exerciseSum={parts} />
         </div>
     )
 }
 
 const Header = (props) => {
-
     return (
         <div>
             <h1>{props.name}</h1>
@@ -37,30 +36,28 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-
+    console.log(props)
     return (
         <div>
-            <Part PartParts={props.Content1} />
-            <Part PartParts={props.Content2} />
-            <Part PartParts={props.Content3} />
+            <Part parts={props.partList[0].name} exes={props.partList[0].exercises} />
+            <Part parts={props.partList[1].name} exes={props.partList[1].exercises} />
+            <Part parts={props.partList[2].name} exes={props.partList[2].exercises} />
         </div>
     )
 }
 
 const Total = (props) => {
-    console.log(props)
     return (
         <div>
-            <p>Number of exercises {props.exerciseSum}</p>
+            <p>Number of exercises {props.exerciseSum[0].exercises + props.exerciseSum[1].exercises + props.exerciseSum[2].exercises}</p>
         </div>
     )
 }
 
 const Part = (props) => {
-
     return (
         <div>
-            <p> {props.PartParts} {props.PartExercises} </p>
+            <p>{props.parts} {props.exes}</p>
         </div>
     )
 }
