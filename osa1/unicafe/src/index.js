ce7fd4215c6d7858d56dd1f024cdfcd1dd5329d5
Sami Åhl <sami.ahl@relex.fi>
2019-11-16 14:53:24 +0200

    osa1: unicafe, tehtävä 7

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -12,11 +12,17 @@ const Button = (props) => {
 
 const Statistics = (props) => {
     console.log(props)
+    const avg = (props.avg / props.allClicks)
+    const pos = (props.good / props.allClicks) * 100
+    console.log(avg)
     return (
         <div>
             <div>Good: {props.good}</div>
             <div>Neutral: {props.neutral}</div>
             <div>Bad: {props.bad}</div>
+            <div>All: {props.allClicks}</div>
+            <div>Average: {avg}</div>
+            <div>Positive: {pos} %</div>
         </div>
     )
 }
@@ -25,17 +31,24 @@ const App = () => {
     const [ good, setGood ] = useState(0)
     const [ neutral, setNeutral ] = useState(0)
     const [ bad, setBad ] = useState(0)
+    const [ allClicks, setAll ] = useState(0)
+    const [ avg, setAvg ] = useState(0)
 
     const handleGoodClick = () => {
         setGood(good + 1)
+        setAll(allClicks + 1)
+        setAvg(avg + 1)
     }
 
     const handleNeutralClick = () => {
         setNeutral(neutral + 1)
+        setAll(allClicks + 1)
     }
 
     const handleBadClick = () => {
         setBad(bad + 1)
+        setAll(allClicks + 1)
+        setAvg(avg - 1)
     }
 
     return (
@@ -46,7 +59,7 @@ const App = () => {
             <Button onClick={handleBadClick} text='bad'/>
 
             <h2>Statistics</h2>
-            <Statistics good={good} neutral={neutral} bad={bad}/>
+            <Statistics good={good} neutral={neutral} bad={bad} allClicks={allClicks} avg={avg}/>
 
         </div>
         )
